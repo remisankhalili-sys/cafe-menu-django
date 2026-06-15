9c474a495b8c32af2bbb24a3e31e79629035a086
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-15 17:25:14 +0330

    Correct the model file.

--- menu/models.py
+++ menu/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
-class Category(models.model):
+
+class Category(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
-class Meta:
-    verbose_name_plural = "Categories"
+
+    class Meta:
+        verbose_name_plural = "Categories"
 
 
 class MenuItem(models.Model):
