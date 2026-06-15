9c5712c04706a10602eabf99953ea04926c81d31
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-15 16:48:06 +0330

    Add ; class Meta , class MenuItem.

--- menu/models.py
+++ menu/models.py
@@ -6,3 +6,17 @@ class Category(models.model):
     def __str__(self):
         return self.name
     
+class Meta:
+    verbose_name_plural = "Categories"
+
+
+class MenuItem(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="items")
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to="menu_items/", blank=True, null=True)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
