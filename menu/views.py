6b386ab0f80cc832d53994fc58b72188d1a1695a
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-17 15:45:14 +0330

    changes views

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Category
 
+
 def menu_home(request):
-    categories = Category.objects.all()
+    categories = Category.objects.prefetch_related(
+        'items__images'
+    ).all()
     return render(request, 'menu/menu_home.html', {'categories': categories})
