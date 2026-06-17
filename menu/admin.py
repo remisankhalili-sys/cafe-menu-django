6bbd0184bf8ef82558052d4e65b5ea8ba046186c
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-17 13:52:15 +0330

    Updated admin panel to manage product image.

--- menu/admin.py
+++ menu/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import Category, MenuItem
+from .models import Category, MenuItem, ProductImage
 
-admin.site.register(Category)
-admin.site.register(MenuItem)
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+    list_display = ['name', 'category', 'price', 'is_available']
+    list_filter = ['category', 'is_available']
+    search_fields = ['name']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['menu_item', 'is_primary']
