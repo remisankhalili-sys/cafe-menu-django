7662efa894a4e01ad09b3ef3b7483dda89969bf3
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-17 13:04:21 +0330

    Add : class ProductImage , import os .

--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 
 class Category(models.Model):
@@ -16,8 +17,21 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    image = models.ImageField(upload_to="menu_items/", blank=True, null=True)
     is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
+
+
+class ProductImage(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="menu_items/")
+    is_primary = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Image for {self.menu_item.name}"
+
+    def delete(self, *args, **kwargs):
+        if self.image and os.path.isfile(self.image.path):
+            os.remove(self.image.path)
+        super().delete(*args, **kwargs)
