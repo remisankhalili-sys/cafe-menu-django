aa404e36f31c13e3cbce353da9d3663c9299108d
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-17 16:17:52 +0330

    Add def menu_item_detail , in views.py.

--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Category
+from django.shortcuts import render, get_object_or_404
+from .models import Category, MenuItem
 
 
 def menu_home(request):
@@ -7,3 +7,14 @@ def menu_home(request):
         'items__images'
     ).all()
     return render(request, 'menu/menu_home.html', {'categories': categories})
+
+
+def menu_item_detail(request, pk):
+    item = get_object_or_404(MenuItem.objects.prefetch_related('images'), pk=pk)
+    primary_image = item.images.filter(is_primary=True).first()
+    other_images = item.images.filter(is_primary=False)
+    return render(request, 'menu/menu_item_detail.html', {
+        'item': item,
+        'primary_image': primary_image,
+        'other_images': other_images,
+    })
