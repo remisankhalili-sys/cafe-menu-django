931b095c7296bbd36db01a09fa8c96ee0b8f1bb0
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-24 17:22:12 +0330

    Add: class Customer(AbstractUser)

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Customer(AbstractUser):
+    """
+    Custom user model extending Django's AbstractUser.
+    Adds phone number and profile picture to the default user fields.
+    Default fields inherited: username, email, password, first_name, last_name
+    """
+
+    phone_number = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True,
+        unique=True
+    )
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.username
