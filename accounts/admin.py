0c2f1655ec3d594ebbef482968c588b40a7012d2
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-06-29 18:34:23 +0330

    Add admin.py in accounts  and add class customerAdmin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Customer
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(UserAdmin):
+    """Admin panel for Customer model."""
+    list_display = ['username', 'email', 'phone_number', 'is_staff', 'is_superuser']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('phone_number', 'profile_picture')}),
+    )
