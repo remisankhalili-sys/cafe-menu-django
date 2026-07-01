4cfd3aa60cf0d53999bb52a4a3ea3015a206a756
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-07-01 17:59:03 +0330

    Update signals.py

--- menu/signals.py
+++ menu/signals.py
@@ -1,18 +1,28 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-import os
-from .models import ProductImage
+from .models import Order, Notification
+from menu.models import ProductImage
+
+
+@receiver(post_save, sender=Order)
+def create_order_notification(sender, instance, created, **kwargs):
+    """Create a notification when a new order is placed."""
+    if created:
+        Notification.objects.create(
+            message=f"New order #{instance.pk} by {instance.customer.username}"
+        )
 
 
 @receiver(pre_save, sender=ProductImage)
-def delete_old_image_on_change(sender, instance, **kwargs):
+def delete_old_image(sender, instance, **kwargs):
+    """Delete old image file from disk when a new image is uploaded."""
     if not instance.pk:
         return
     try:
-        old_image = ProductImage.objects.get(pk=instance.pk).image
+        old_image = ProductImage.objects.get(pk=instance.pk)
     except ProductImage.DoesNotExist:
         return
-    new_image = instance.image
-    if old_image and old_image != new_image:
-        if os.path.isfile(old_image.path):
-            os.remove(old_image.path)
+    if old_image.image != instance.image:
+        import os
+        if os.path.isfile(old_image.image.path):
+            os.remove(old_image.image.path)
